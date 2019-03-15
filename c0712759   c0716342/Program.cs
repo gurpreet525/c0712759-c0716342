c5da7eda23650c879a1c0bd3ea504918405bd9a8
visualstudio <visualstudio@122-PC34>
2019-03-15 18:04:57 -0400

    final commit

--- c0712759   c0716342/Program.cs
+++ c0712759   c0716342/Program.cs
@@ -6,7 +6,9 @@
 
 namespace BankAccountNS
 {
-    
+    /// <summary>
+    /// Bank Account demo class.
+    /// </summary>
     public class BankAccount
     {
         private string m_customerName;
@@ -24,6 +26,7 @@ namespace BankAccountNS
             m_customerName = customerName;
             m_balance = balance;
         }
+
         public string CustomerName
         {
             get { return m_customerName; }
@@ -33,6 +36,7 @@ namespace BankAccountNS
         {
             get { return m_balance; }
         }
+
         public void Debit(double amount)
         {
             if (m_frozen)
@@ -53,6 +57,38 @@ namespace BankAccountNS
             m_balance += amount; // intentionally incorrect code
         }
 
+        public void Credit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+        private void FreezeAccount()
+        {
+            m_frozen = true;
+        }
+
+        private void UnfreezeAccount()
+        {
+            m_frozen = false;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
     }
 }
-
